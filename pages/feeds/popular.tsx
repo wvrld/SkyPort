d2d2cd30597113f446bf027d50dd03129de9ff22
wvrld <lukas.vojtisek@semantic-visions.com>
2023-07-26 00:21:43 +0200

    fixed popular feeds

--- pages/feeds/popular.tsx
+++ pages/feeds/popular.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
 import { AppBskyFeedDefs } from '@atproto/api'
 import { OutputSchema } from '@atproto/api/dist/client/types/app/bsky/unspecced/getPopularFeedGenerators'
 import { t } from '@lingui/macro'
 import { useQuery } from '@tanstack/react-query'
 import { BrightStar, Heart } from 'iconoir-react'
 import Link from 'next/link'
+import { useState } from 'react'
 import { useLocalStorage } from 'usehooks-ts'
 
 import Avatar from '@/components/Avatar'
@@ -47,7 +47,7 @@ export default function PopularFeeds(): JSX.Element {
           {/* Add search input and button */}
           <div className="mb-4 flex items-center">
             <input
-              className="border rounded-md p-2 flex-grow mr-2"
+              className="mr-2 flex-grow rounded-md border p-2"
               type="text"
               placeholder="Search feeds..."
               value={search}
@@ -56,71 +56,72 @@ export default function PopularFeeds(): JSX.Element {
             <Button>Search</Button>
           </div>
 
+          {data?.feeds
+            ?.filter(feed => feed.displayName.toLowerCase().includes(search.toLowerCase()))
+            .map(feed => {
+              const feedName = feed.uri.split('/').at(-1)
+              const feedPath = `/u/${feed.creator.handle}/feed/${feedName}`
+              const isFollowing = feeds?.find(f => f.cid === feed.cid)
 
-          {data?.feeds?.filter(feed => feed.displayName.toLowerCase().includes(search.toLowerCase())).map(feed => {
-            const feedName = feed.uri.split('/').at(-1)
-            const feedPath = `/u/${feed.creator.handle}/feed/${feedName}`
-            const isFollowing = feeds?.find(f => f.cid === feed.cid)
-
-            return (
-              <Link
-                href={feedPath}
-                className="block border-b px-4 py-6 last:border-none hover:bg-gray-50 dark:hover:bg-slate-900"
-                key={feed.cid}
-              >
-                <div className="flex items-start space-x-3">
-                  <div className="flex w-12">
-                    {feed.avatar ? (
-                      <>
-                        {/*eslint-disable-next-line @next/next/no-img-element*/}
-                        <img className="h-10 w-10 rounded-full" src={feed.avatar} alt={feed.displayName} />
-                      </>
-                    ) : null}
-                  </div>
-
-                  <div className="flex-1 grow space-y-3">
-                    <div className="space-y-1">
-                      <div className="font-semibold">{feed.displayName}</div>
-                      <div className="break-all text-sm text-zinc-500">{feed.description}</div>
+              return (
+                <Link
+                  href={feedPath}
+                  className="block border-b px-4 py-6 last:border-none hover:bg-gray-50 dark:hover:bg-slate-900"
+                  key={feed.cid}
+                >
+                  <div className="flex items-start space-x-3">
+                    <div className="flex w-12">
+                      {feed.avatar ? (
+                        <>
+                          {/*eslint-disable-next-line @next/next/no-img-element*/}
+                          <img className="h-10 w-10 rounded-full" src={feed.avatar} alt={feed.displayName} />
+                        </>
+                      ) : null}
                     </div>
 
-                    <div className="flex items-center space-x-6">
-                      <div className="flex items-center space-x-1">
-                        <Heart className="h-4 w-4" />
-                        <div className="text-xs font-medium">{feed.likeCount}</div>
+                    <div className="flex-1 grow space-y-3">
+                      <div className="space-y-1">
+                        <div className="font-semibold">{feed.displayName}</div>
+                        <div className="break-all text-sm text-zinc-500">{feed.description}</div>
                       </div>
 
-                      <div className="flex items-center space-x-2 text-sm">
-                        <Avatar {...feed.creator} className="h-5 w-5 rounded-full" hoverCard />
-                        <Username {...feed.creator} />
+                      <div className="flex items-center space-x-6">
+                        <div className="flex items-center space-x-1">
+                          <Heart className="h-4 w-4" />
+                          <div className="text-xs font-medium">{feed.likeCount}</div>
+                        </div>
+
+                        <div className="flex items-center space-x-2 text-sm">
+                          <Avatar {...feed.creator} className="h-5 w-5 rounded-full" hoverCard />
+                          <Username {...feed.creator} />
+                        </div>
                       </div>
                     </div>
+
+                    <div>
+                      <Button
+                        variant={isFollowing ? 'secondary' : 'default'}
+                        onClick={e => {
+                          e.preventDefault()
+                          e.stopPropagation()
+
+                          if (isFollowing) {
+                            setFeeds(feeds.filter(f => f.cid !== feed.cid))
+
+                            return
+                          }
+
+                          setFeeds([...feeds, feed])
+                        }}
+                        size="sm"
+                      >
+                        {isFollowing ? t`Unfollow` : t`Follow`}
+                      </Button>
+                    </div>
                   </div>
-
-                  <div>
-                    <Button
-                      variant={isFollowing ? 'secondary' : 'default'}
-                      onClick={e => {
-                        e.preventDefault()
-                        e.stopPropagation()
-
-                        if (isFollowing) {
-                          setFeeds(feeds.filter(f => f.cid !== feed.cid))
-
-                          return
-                        }
-
-                        setFeeds([...feeds, feed])
-                      }}
-                      size="sm"
-                    >
-                      {isFollowing ? t`Unfollow` : t`Follow`}
-                    </Button>
-                  </div>
-                </div>
-              </Link>
-            )
-          })}
+                </Link>
+              )
+            })}
         </div>
       </Page>
     </div>
@@ -145,4 +146,3 @@ function FeedSkeleton(): JSX.Element {
     </div>
   )
 }
-
